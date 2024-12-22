42da093a24e2e566500661ae5c626838505c6ef2
Палецкая Лиза <73171723+yey3@users.noreply.github.com>
2024-12-22 22:38:47 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+﻿#include "Matrix.h"
+#include "MatrixOperations.h"
+#include "MenuManager.h"
+#include <windows.h>
+#include <iostream>
+using namespace std;
+
+
+int main() {
+	setlocale(0, "");
+	MenuManager mm;
+	mm.menu();
+}
